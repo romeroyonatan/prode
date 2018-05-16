232d2ab66ba37c8b6d9c9834dc3f23fd3c8c9276
Yonatan Romero <romeroyonatan@gmail.com>
2018-05-16 13:06:24 -0300

    Crea modelos de apuestas

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,7 +70,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'prode.users.apps.UsersConfig',
-    # Your stuff: custom apps go here
+    'prode.apuestas.apps.ApuestasConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ prode/apuestas/tests/factories.py
@@ -0,0 +1,40 @@
+import factory
+import factory.fuzzy
+
+from django.utils import timezone
+
+from prode.apuestas import models
+from prode.users.tests import factories as user_factories
+
+
+class EtapaFactory(factory.django.DjangoModelFactory):
+    nombre = factory.Sequence(lambda n: f"etapa-{n}")
+    vencimiento = factory.fuzzy.FuzzyDateTime(timezone.now())
+    slug = factory.Sequence(lambda n: f"etapa-{n}")
+    publica = True
+
+    class Meta:
+        model = "apuestas.Etapa"
+
+
+class PartidoFactory(factory.django.DjangoModelFactory):
+    local = factory.Faker('country_code')
+    visitante = factory.Faker('country_code')
+    goles_local = factory.Faker('random_digit_not_null')
+    goles_visitante = factory.Faker('random_digit_not_null')
+
+    class Meta:
+        model = "apuestas.Partido"
+
+
+class ApuestaFactory(factory.django.DjangoModelFactory):
+    usuario = factory.SubFactory(user_factories.UserFactory)
+    partido = factory.SubFactory(PartidoFactory)
+    ganador = factory.fuzzy.FuzzyChoice((models.EMPATE,
+                                         models.GANA_LOCAL,
+                                         models.GANA_VISITANTE))
+    goles_local = factory.Faker('random_digit_not_null')
+    goles_visitante = factory.Faker('random_digit_not_null')
+
+    class Meta:
+        model = "apuestas.Apuesta"
